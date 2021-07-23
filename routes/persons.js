5af9f5ddd5ff633ef148141a81945f6086a32023
Athameon <Benjamin.Nopper@freenet.de>
2021-07-23 10:16:51 +0200

    Added validation for person POST and PUT.

--- routes/persons.js
+++ routes/persons.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const personController = require("../controllers/personController");
 const { body } = require("express-validator");
 
+const validateUserValues = [
+  //email,password,firstName,lastName
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  body("firstName").isLength({ min: 3 }),
+  body("lastName").isLength({ min: 3 }),
+];
+
 router.get("/", personController.getAllPersons);
 router.get("/:id", personController.getSinglePerson);
-router.post("/", personController.createNewPerson);
-router.put("/:id", personController.updatePerson);
-router.patch("/userLevel/:id", personController.setRole);
+router.post("/", validateUserValues, personController.createNewPerson);
+router.put("/:id", validateUserValues, personController.updatePerson);
+router.patch("/role/:id", personController.setRole);
 router.delete("/:id", personController.deletePerson);
 
 module.exports = router;

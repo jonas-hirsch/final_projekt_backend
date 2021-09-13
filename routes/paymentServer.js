0459cfdb91631b61104aa0a6c1bc02c0fa1f67f0
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-09-13 17:14:11 +0200

    bugfix

--- routes/paymentServer.js
+++ routes/paymentServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const payment = require("../controllers/paymentController")
 
-// app.use(express.static("."));
+router.use(express.static("."));
 
 
 router.get("/:id", payment.createPayment);
